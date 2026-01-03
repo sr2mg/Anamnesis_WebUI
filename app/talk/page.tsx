7a483165e3220ce1e05b9d57d1775f28e706ea4d
kun432 <kun432@users.noreply.github.com>
2026-01-03 18:27:22 +0900

    環境変数を実行時に読むようにした

--- app/talk/page.tsx
+++ app/talk/page.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import TalkClient from './TalkClient';
 
+export const dynamic = 'force-dynamic';
+
 export default function TalkPage() {
     const envApiKey = process.env.GOOGLE_API_KEY;
 
